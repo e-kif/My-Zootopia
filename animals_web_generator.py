5202ac7a84cb5ca2d2a1e50589f04b4e65e9c733
e-kif <e.kifiuk@gmail.com>
2024-08-02 17:23:28 +0300

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,7 +10,27 @@ def load_data(file_path):
         return json.loads(file_obj.read())
 
 
-def print_animal_info(animals_data):
+def read_html_template(file_path):
+    """Loads content of html template
+    :param file_path: path to a html file
+    :return: string
+    """
+    with open(file_path, "r") as file:
+        return file.read()
+
+
+def write_html_file(file_path, html_content):
+    """Writes given html_content to a file
+    :param file_path: html file to be written
+    :param html_content: string
+    :return: None
+    """
+    with open(file_path, "w") as file:
+        file.write(html_content)
+
+
+def get_all_animals_info(animals_data):
+    all_animals_info = ""
     for animal in animals_data:
         animal_info = {
             'name': animal['name'],
@@ -23,14 +43,17 @@ def print_animal_info(animals_data):
             pass
 
         for info in animal_info.keys():
-            print(f'{info.capitalize()}: {animal_info[info]}')
-        print()
+            all_animals_info += f'{info.capitalize()}: {animal_info[info]}\n'
+        all_animals_info += '\n'
+    return all_animals_info
 
 
 def main():
     data = load_data("animals_data.json")
-    print_animal_info(data)
-
+    html_template = read_html_template('animals_template.html')
+    animals_info_string = get_all_animals_info(data)
+    new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_info_string)
+    write_html_file('animals.html', new_html)
 
 if __name__ == "__main__":
     main()
